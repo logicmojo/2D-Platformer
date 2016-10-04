9f7fd630d3b0d017f8bd911a171a84fe03f2a84d
logicmojo <jordankwexler@gmail.com>
2016-10-04 09:12:35 -0400

    2D platformer scripts
    As of 10/4/16

---
+++ Player.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Controller2D))]
+public class Player : MonoBehaviour
+{
+
+    public float jumpHeight = 4;
+    public float timeToJumpApex = .4f;
+
+    float accelerationTimeAirborne = .2f;
+    float accelerationTimeGrounded = .1f;
+    float moveSpeed = 6;
+
+    bool doubleJump = false;
+    float gravity;
+    float jumpVelocity;
+    Vector3 velocity;
+    float velocityXSmoothing;
+
+    Controller2D controller;
+
+    void Start()
+    {
+        controller = GetComponent<Controller2D>();
+
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+        //print("Gravity: " + gravity + "  Jump Velocity: " + jumpVelocity);
+    }
+
+    void FixedUpdate()
+    {
+        if (controller.collisions.below)
+        {
+            doubleJump = false;
+        }
+    }
+
+    void Update()
+    {
+
+        if (controller.collisions.above || controller.collisions.below)
+        {
+            velocity.y = 0;
+        }
+
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetKeyDown(KeyCode.Space) && (controller.collisions.below || !doubleJump))
+        {
+
+            velocity.y = jumpVelocity;
+
+            if (!doubleJump && !controller.collisions.below)
+            {
+                doubleJump = true;
+            }
+
+
+        }
+
+        float targetVelocityX = input.x * moveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
